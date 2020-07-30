670a54adcead6dfb8cbd97a07f77c746e3e5e596
Jesse Wellenberg <jesse.wellenberg@axians.com>
2020-07-30 21:56:24 +0200

    configure bundles

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,16 @@ import pkg from "./package.json";
 export default {
     input: "src/index.ts",
     plugins: [typescript({tsconfig: "./tsconfig.json"})],
-    output: {
-      dir: "dist", format: "es",
-    },
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+    ],
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
